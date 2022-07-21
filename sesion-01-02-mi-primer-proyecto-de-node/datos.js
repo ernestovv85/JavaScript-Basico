a5a51420efcabfe27b89732fee83f5dd49de6e4a
ernestovv85 <ernestovv85@gmail.com>
2022-07-20 21:12:42 -0500

    Ejercicio terminado

--- sesion-01-02-mi-primer-proyecto-de-node/datos.js
+++ sesion-01-02-mi-primer-proyecto-de-node/datos.js
@@ -1,6 +1,6 @@
 const lista = ['ernesto', 36, false, new Date('08/09/1985'), {
   titulo: 'La biblia de los caídos',
   autor: 'Fernando Trujillo Sanz',
-  fecha: 2014,
+  fecha: new Date('2014'),
   url: 'https://lektu.com/l/fernando-trujillo/la-biblia-de-los-caidos/467'
 }];
