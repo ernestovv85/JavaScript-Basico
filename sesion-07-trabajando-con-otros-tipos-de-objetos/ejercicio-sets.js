f89920880c89392f7723f86b985bc6b1d2f5bce9
ernestovv85 <ernestovv85@gmail.com>
2022-07-21 15:23:15 -0500

    Ejercicio sets

---
+++ sesion-07-trabajando-con-otros-tipos-de-objetos/ejercicio-sets.js
@@ -0,0 +1,12 @@
+let nombres = ['juan', 'paco', 'pedro'];
+
+let setNombres = new Set(nombres);
+
+setNombres.add ('ernesto');
+console.log(setNombres);
+
+setNombres.add ('ernesto');
+console.log(setNombres);
+
+setNombres.add ('Javascript');
+console.log(setNombres);
