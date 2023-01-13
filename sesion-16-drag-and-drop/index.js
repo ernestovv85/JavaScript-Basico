d898bb4932aa1549f9b0e899a6424e6d78449e4a
ernestovv85 <ernestovv85@gmail.com>
2023-01-13 15:54:50 -0600

    ejercicio completado

--- sesion-16-drag-and-drop/index.js
+++ sesion-16-drag-and-drop/index.js
@@ -1,34 +1,39 @@
-const parrafos = document.querySelectorAll(".parrafo")
-const secciones = document.querySelectorAll(".seccion")
+const parrafos = document.querySelectorAll(".parrafo");
+const secciones = document.querySelectorAll(".seccion");
+const wasted = document.querySelector(".waste");
+let droped = false;
 
-parrafos.forEach(parrafo => {
-    parrafo.addEventListener("dragstart", event => {
-        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
-        parrafo.classList.add("dragging")
-        event.dataTransfer.setData("id", parrafo.id)
-        const elemento_fantasma = document.querySelector(".imagen-fantasma")
-        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
-    })
+parrafos.forEach((parrafo) => {
+  parrafo.addEventListener("dragstart", (event) => {
+    console.log("Estoy arrastrando el párrafo: " + parrafo.innerText);
+    parrafo.classList.add("dragging");
+    event.dataTransfer.setData("id", parrafo.id);
+    const elemento_fantasma = document.querySelector(".imagen-fantasma");
+    event.dataTransfer.setDragImage(elemento_fantasma, 0, 0);
+  });
 
-    parrafo.addEventListener("dragend", () => {
-        // console.log("He terminado de arrastrar")
-        parrafo.classList.remove("dragging")
-    })
-})
+  parrafo.addEventListener("dragend", () => {
+    // console.log("He terminado de arrastrar")
+    parrafo.classList.remove("dragging");
+    wasted.appendChild(parrafo);
+  });
+});
 
-secciones.forEach(seccion => {
-    seccion.addEventListener("dragover", event => {
-        event.preventDefault()
-        event.dataTransfer.dropEffect = "copy" // copy por defecto
-        // console.log("Drag Over")
-        //
-    })
+secciones.forEach((seccion) => {
+  seccion.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy"; // copy por defecto
+    // console.log("Drag Over")
+    //
+  });
 
-    seccion.addEventListener("drop", event => {
-        console.log("Drop")
-        const id_parrafo = event.dataTransfer.getData("id")
-        // console.log("Párrafo id: ", id_parrafo)
-        const parrafo = document.getElementById(id_parrafo)
-        seccion.appendChild(parrafo)
-    })
-})
+  seccion.addEventListener("drop", (event) => {
+    event.preventDefault
+    console.log("Drop");
+    const id_parrafo = event.dataTransfer.getData("id");
+    // console.log("Párrafo id: ", id_parrafo)
+    const parrafo = document.getElementById(id_parrafo);
+    seccion.appendChild(parrafo);
+  });
+});
+
